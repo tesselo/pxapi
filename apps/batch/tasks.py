541ab3a5b97179b9e8298ad00e31c8b279c337fc
Daniel Wiesmann <daniel@tesselo.com>
2021-02-07 22:36:56 +0000

    Fixed active job update routine, added logging to task.

--- apps/batch/tasks.py
+++ apps/batch/tasks.py
@@ -1,12 +1,21 @@
+import logging
+
 from batch.models import BatchJob
+
+logger = logging.getLogger(__name__)
 
 
 def scan_active_tasks():
     """
-    Refresh job status and description until they reach a non-active status.
+    Refresh job status and description.
+
+    Active jobs are jobs that do not have an unknown state, or that have
+    finished (successfully or not). The job status is updated regularly until
+    the job reaches a non-active status.
     """
     active_jobs = BatchJob.objects.exclude(
-        status__in=[BatchJob.UNKNOWN, BatchJob.FAILED, BatchJob.FAILED]
+        status__in=[BatchJob.UNKNOWN, BatchJob.SUCCEEDED, BatchJob.FAILED]
     )
+    logger.info("Updating active jobs. Found {}.".format(active_jobs.count()))
     for job in active_jobs:
         job.update()
