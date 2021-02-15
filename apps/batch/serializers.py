cc287df8726429950cc72d505084b51ca5fe85c9
Daniel Wiesmann <daniel@tesselo.com>
2021-02-15 18:13:26 +0000

    Simplified batch job serializer.

--- apps/batch/serializers.py
+++ apps/batch/serializers.py
@@ -1,27 +1,8 @@
-import json
-
-from jobs.models import BatchJob
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from batch.models import BatchJob
+from rest_framework.serializers import ModelSerializer
 
 
 class BatchJobSerializer(ModelSerializer):
-
-    description = SerializerMethodField()
-
     class Meta:
         model = BatchJob
-        fields = (
-            "id",
-            "job_id",
-            "status",
-            "created",
-            "log_stream_name",
-            "description",
-        )
-
-    def get_description(self, obj):
-        try:
-            desc = json.loads(obj.description)
-        except json.decoder.JSONDecodeError:
-            desc = "Could not get description. JSON decode error."
-        return desc
+        fields = "__all__"
