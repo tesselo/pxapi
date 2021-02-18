2c0c02d9f5b85f0df3f72e2282e15899617c87c7
Daniel Wiesmann <daniel@tesselo.com>
2021-02-18 00:37:51 +0000

    Tried alternative sys path.

--- pxapi/wsgi.py
+++ pxapi/wsgi.py
@@ -13,8 +13,11 @@ from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../apps'))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pxapi.settings")
 
