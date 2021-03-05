4afdd47bdb04c42bbb0680ec02573debb747639c
Daniel Wiesmann <daniel@tesselo.com>
2021-03-05 16:04:02 +0000

    Improved CI -- Added tests, and build on PR, deploy on push to main.

--- pxapi/urls.py
+++ pxapi/urls.py
@@ -20,12 +20,14 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from rest_framework.authtoken import views
 
 from pxapi.apiurls import apiurlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("api-token-auth/", views.obtain_auth_token, name="api-token-login"),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "swagger-ui/",
