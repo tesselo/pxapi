91c172c927ff627e40b4a377bf3d99f3a6dfca7f
Daniel Wiesmann <daniel@tesselo.com>
2021-03-26 11:30:28 +0000

    Registered predictions in the django admin.

--- apps/pipeline/admin.py
+++ apps/pipeline/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
-from pipeline.models import KerasModel, PixelsData, TrainingData
+from pipeline.models import KerasModel, PixelsData, Prediction, TrainingData
 
 admin.site.register(TrainingData, GuardedModelAdmin)
 admin.site.register(PixelsData, GuardedModelAdmin)
 admin.site.register(KerasModel, GuardedModelAdmin)
+admin.site.register(Prediction, GuardedModelAdmin)
