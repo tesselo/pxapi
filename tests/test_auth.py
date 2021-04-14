c19dc5dc65116a63ec1004ffd2e1a92ded4701aa
Daniel Wiesmann <daniel@tesselo.com>
2021-04-14 14:30:40 +0100

    Cleaned auth test.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,17 +7,18 @@ from rest_framework.reverse import reverse
 
 class ApiAuthViewTests(TestCase):
     def setUp(self):
+        # Create test user.
         User = get_user_model()
         self.usr = User.objects.create_user(
             username="michael", email="michael@bluth.com", password="bananastand"
         )
 
-        self.login_url = reverse("api-token-login")
-
     def test_login(self):
+        # Get login url.
+        login_url = reverse("api-token-login")
         # Login.
         response = self.client.post(
-            self.login_url, data={"username": "michael", "password": "bananastand"}
+            login_url, data={"username": "michael", "password": "bananastand"}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(Token.objects.filter(user=self.usr).exists())
