c303c86f7099fd4331d2aec7cd830169c20ad7aa
Daniel Wiesmann <daniel@tesselo.com>
2021-04-16 10:17:30 +0100

    Increased max number of jobs for prediction to 1000.

--- apps/pipeline/models.py
+++ apps/pipeline/models.py
@@ -370,7 +370,7 @@ class Prediction(NamedModel):
             number_of_items = get_catalog_length(self.pixelsdata.collection_uri)
             # TODO: Item per job definition.
             # Set number of jobs based on catolog length, with maximun ceiling.
-            max_number_jobs = 100
+            max_number_jobs = 1000
             item_per_job = 10
             number_of_jobs = math.ceil(number_of_items / item_per_job)
             if number_of_jobs > max_number_jobs:
