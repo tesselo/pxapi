071ba370c272676bf5859bb4896dcbd8d04088bd
Daniel Wiesmann <daniel@tesselo.com>
2021-08-06 09:56:02 +0100

    Added status to admin list view for pipeline and batchjob objects lists.

--- apps/batch/admin.py
+++ apps/batch/admin.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 class BatchJobModelAdmin(admin.ModelAdmin):
     search_fields = ["job_id"]
     list_filter = ["status"]
+    list_display = ["job_id", "status"]
 
 
 admin.site.register(BatchJob, BatchJobModelAdmin)
