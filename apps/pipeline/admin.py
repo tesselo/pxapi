0bc53dbe4cc41340e1e4bb850fce30d9e0822eb1
Daniel Wiesmann <daniel@tesselo.com>
2021-08-09 13:40:43 +0100

    Added auto-update routine to pipeline admin view.

--- apps/pipeline/admin.py
+++ apps/pipeline/admin.py
@@ -1,3 +1,4 @@
+from batch.models import BatchJob
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
 from pipeline.models import KerasModel, PixelsData, Prediction, TrainingData
@@ -8,20 +9,20 @@ class NamedGuardedModelAdmin(GuardedModelAdmin):
     list_display = ["name", "status"]
     change_form_template = "change_form.html"
 
-    status_key = None
+    status_keys = None
 
     def status(self, obj):
         """
         Obtain the status from a related batchjob object.
 
-        The status_key property is the name of the ForeignKey field linking to
+        The status_keys property is the name of the ForeignKey field linking to
         the batch job object.
         """
         # Ensure key was set.
-        if not self.status_key:
+        if not self.status_keys:
             raise NotImplementedError("Status key needs to be specified.")
-        # Get related batch job using the key string.
-        batchjob = getattr(obj, self.status_key, None)
+        # Get related batch job using the key string of the last job in list.
+        batchjob = getattr(obj, self.status_keys[-1], None)
         # If the batchjob object exists, use its status for display. Otherwise
         # return unknown.
         if batchjob:
@@ -29,29 +30,56 @@ class NamedGuardedModelAdmin(GuardedModelAdmin):
         else:
             return "Unknown"
 
+    def get_queryset(self, request):
+        """
+        Overriding the queryset for the pipeline admin views to update tasks on
+        page load.
+        """
+        # Get original queryset.
+        queryset = super().get_queryset(request)
+        # Loop through related batch job keys.
+        for status_key in self.status_keys:
+            # Reduce objects with running jobs.
+            running_queryset = queryset.exclude(
+                **{
+                    f"{status_key}__status__in": [
+                        BatchJob.UNKNOWN,
+                        BatchJob.SUCCEEDED,
+                        BatchJob.FAILED,
+                    ]
+                }
+            )
+            # For all the running jobs, update status.
+            for obj in running_queryset:
+                batchjob = getattr(obj, status_key, None)
+                if batchjob:
+                    batchjob.update()
+        # Return default queryset.
+        return queryset
+
 
 class TrainingDataAdmin(NamedGuardedModelAdmin):
     list_filter = ["batchjob_parse__status"]
-    status_key = "batchjob_parse"
+    status_keys = ["batchjob_parse"]
     readonly_fields = ["batchjob_parse"]
 
 
 class PixelsDataAdmin(NamedGuardedModelAdmin):
     list_filter = ["batchjob_collect_pixels__status", "batchjob_create_catalog__status"]
-    status_key = "batchjob_create_catalog"
+    status_keys = ["batchjob_collect_pixels", "batchjob_create_catalog"]
     readonly_fields = ["batchjob_collect_pixels", "batchjob_create_catalog"]
 
 
 class KerasModelAdmin(NamedGuardedModelAdmin):
     list_filter = ["batchjob_train__status"]
     change_form_template = "change_form_kerasmodel.html"
-    status_key = "batchjob_train"
+    status_keys = ["batchjob_train"]
     readonly_fields = ["batchjob_train"]
 
 
 class PredictionAdmin(NamedGuardedModelAdmin):
     list_filter = ["batchjob_predict__status", "batchjob_create_catalog__status"]
-    status_key = "batchjob_create_catalog"
+    status_keys = ["batchjob_predict", "batchjob_create_catalog"]
     readonly_fields = ["batchjob_predict", "batchjob_create_catalog"]
 
 
